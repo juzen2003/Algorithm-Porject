ab0679d7bb92aee9783b2edd337154ee90a0bec8
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-01 21:19:44 -0700

    Done phase 2

--- yujen_chang_2/lib/p02_hashing.rb
+++ yujen_chang_2/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    hash_val = 0
+    self.each_with_index do |el, idx|
+      hash_val += ((idx + 1)*10) ^ el.hash
+    end
+
+    hash_val
   end
 end
 
 class String
   def hash
+    hash_val = 0
+    self.chars.each_with_index do |chr, idx|
+      hash_val += ((idx + 1)*10) ^ chr.ord.hash
+    end
+
+    hash_val
   end
 end
 
@@ -16,6 +28,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_val = 0
+    self.each do |k, v|
+      hash_val += (k.hash * v.hash)
+    end
+
+    hash_val
   end
 end
