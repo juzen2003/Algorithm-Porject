6534aabe18069de205e3f6acacbc6d4c8e485c58
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-01 21:31:48 -0700

    Done phase 3

--- yujen_chang_2/lib/p03_hash_set.rb
+++ yujen_chang_2/lib/p03_hash_set.rb
@@ -9,18 +9,26 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count >= num_buckets
+    self[key] << key if !self.include?(key)
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +36,15 @@ class HashSet
   end
 
   def resize!
+    new_num_buckets = 2 * num_buckets
+    new_store = Array.new(new_num_buckets) { Array.new }
+
+    @store.each do |bucket|
+      bucket.each do |key|
+        new_store[key % new_num_buckets] << key
+      end
+    end
+
+    @store = new_store
   end
 end
