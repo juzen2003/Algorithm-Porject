4f0fbd1a4ba6329c8bdfba844446c8a11ae8ae77
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-02 08:56:05 -0700

    Done phase 7

--- yujen_chang_2/lib/p07_practical.rb
+++ yujen_chang_2/lib/p07_practical.rb
@@ -1,5 +1,31 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  hsh = HashMap.new
+
+  string.chars.each do |chr|
+    if hsh[chr] != nil
+      hsh[chr] += 1
+    else
+      hsh[chr] = 1
+    end
+  end
+
+  str_length = string.length
+  odd_el_num = 0
+
+  hsh.each do |chr, count|
+    # even string
+    if str_length % 2 == 0
+      odd_el_num += 1 if count % 2 == 1
+      return false if odd_el_num != 0
+    else # odd string
+      odd_el_num += 1 if count % 2 == 1
+      return false if odd_el_num > 1
+    end
+  end
+
+  true
+
+  # p hsh
 end
