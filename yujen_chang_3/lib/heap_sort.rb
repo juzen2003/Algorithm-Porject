b68763faab12db84c4d36059abd2e323218ad255
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-02 23:34:04 -0700

    Done k_largest_elements

--- yujen_chang_3/lib/heap_sort.rb
+++ yujen_chang_3/lib/heap_sort.rb
@@ -2,11 +2,12 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    prc = Proc.new {|el1, el2| el2 <=> el1}
     # heapify_up
     # set the artifical line from left to right
     i = 1
     while i < self.length
-      BinaryMinHeap.heapify_up(self, i, i+1)
+      BinaryMinHeap.heapify_up(self, i, i+1, &prc)
       i += 1
     end
 
@@ -18,11 +19,11 @@ class Array
     j = self.length-1
     while j > 0
       self[j], self[0] = self[0], self[j]
-      BinaryMinHeap.heapify_down(self, 0, j)
+      BinaryMinHeap.heapify_down(self, 0, j, &prc)
       j -= 1
       # p "extract: #{self}"
     end
 
-    self.reverse!
+    self
   end
 end
--- yujen_chang_3/lib/k_largest_elements.rb
+++ yujen_chang_3/lib/k_largest_elements.rb
@@ -1,5 +1,32 @@
 require_relative 'heap'
+require_relative 'heap_sort'
 
 def k_largest_elements(array, k)
+  # push k els of array into BinaryMinHeap
+  # iterate k times
+  # push into heap O(logk)
+  # this operation is O(klogk)
+  heap = BinaryMinHeap.new
+  k.times do
+    heap.push(array.pop)
+  end
+
+  # push & extract rest of array (n-k)
+  # push into heap O(log(n-k))
+  # extract from heap O(log(n-k))
+  # O((n-k)log(n-k))
+  while !array.empty?
+    heap.push(array.pop)
+    heap.extract
+  end
+
+  # time: O(nlogn)
+  heap.store
+
+  # using heap_sort
+  # O(nlogn) for heap sort
+  # O(n)
+  # array.heap_sort!
+  # array.drop(array.length - k)
 
 end
