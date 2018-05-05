aebca129e1aeb57a6186985749a2b3f151a28b2c
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-05 11:47:13 -0700

    Done bst practical

--- yujen_chang_5/lib/bst_practical.rb
+++ yujen_chang_5/lib/bst_practical.rb
@@ -1,3 +1,16 @@
-
 def kth_largest(tree_node, k)
+  arr = []
+  arr = in_order_traversal(tree_node, arr)
+  arr[k-1]
+end
+
+# order: large to small
+def in_order_traversal(tree_node, arr = [])
+  return arr if tree_node == nil
+
+  arr = in_order_traversal(tree_node.right, arr)
+  arr << tree_node
+  arr = in_order_traversal(tree_node.left, arr)
+
+  arr
 end
