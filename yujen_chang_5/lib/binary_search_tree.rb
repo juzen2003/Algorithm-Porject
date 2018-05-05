fc83a39be2f71c5e063c7d4808b59109421e2ec0
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-05 00:18:07 -0700

    Implement delete

--- yujen_chang_5/lib/binary_search_tree.rb
+++ yujen_chang_5/lib/binary_search_tree.rb
@@ -1,25 +1,90 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
 
 class BinarySearchTree
+  attr_accessor :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if @root == nil
+      @root = BSTNode.new(value)
+    else
+      insert_node(@root, value)
+    end
   end
 
   def find(value, tree_node = @root)
+    return nil if tree_node == nil
+
+    if tree_node.value == value
+      tree_node
+    elsif tree_node.value > value
+      find(value, tree_node.left)
+    else
+      find(value, tree_node.right)
+    end
   end
 
   def delete(value)
+    target_node = find(value, node=@root)
+    parent = target_node.parent
+    # p "T NODE: ", target_node
+    # p "NODE:", node
+    return @root = nil if target_node == @root
+    return nil if target_node == nil
+
+    if (target_node.left == nil && target_node.right == nil)
+      if target_node.value > parent.value
+        parent.right = nil
+      else
+        parent.left = nil
+      end
+    elsif (target_node.left != nil && target_node.right == nil)
+      parent.left = target_node.left
+      target_node.left.parent = parent
+
+    elsif (target_node.left == nil && target_node.right != nil)
+      parent.right = target_node.right
+      target_node.right.parent = parent
+      
+    else
+      left_max_node = maximum(target_node.left)
+      # if left_max_node has left children, it would take left_max_node old place
+      if left_max_node.left != nil
+        left_max_node.parent.right = left_max_node.left
+        left_max_node.left.parent = left_max_node.parent
+      end
+
+      # replace target_node with left_max_node
+      parent.left = left_max_node
+      left_max_node.parent = parent
+      left_max_node.left = target_node.left
+      left_max_node.right = target_node.right
+    end
+
+    # remove the target_node from parent
+    parent.remove(target_node)
+    # target_node
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    if tree_node.right.nil?
+      tree_node
+    else
+      # keep checking the right nodes
+      maximum(tree_node.right)
+    end
+
   end
 
   def depth(tree_node = @root)
-  end 
+
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -30,5 +95,18 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+  def insert_node(node, value)
+    return BSTNode.new(value) if node == nil
+
+    if (node.value >= value)
+      node.left = insert_node(node.left, value)
+      node.left.parent = node
+    else
+      node.right = insert_node(node.right, value)
+      node.right.parent = node
+    end
+
+    node
+  end
 
 end
--- yujen_chang_5/lib/bst_node.rb
+++ yujen_chang_5/lib/bst_node.rb
@@ -1,10 +1,19 @@
 class BSTNode
-  attr_accessor :left, :right
+  attr_accessor :left, :right, :parent
   attr_reader :value
-  
+
   def initialize(value)
     @value = value
     @left = nil
     @right = nil
+    @parent = nil
+  end
+
+  def remove(child)
+    if child == @left
+      @left = nil
+    elsif child == @right
+      @right = nil
+    end
   end
 end
