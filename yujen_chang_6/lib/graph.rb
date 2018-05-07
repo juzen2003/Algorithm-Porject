dce9bb7fc195b7ea79246114c856c3ecb0add878
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-07 09:47:54 -0700

    Done initialize edge & vertex

--- yujen_chang_6/lib/graph.rb
+++ yujen_chang_6/lib/graph.rb
@@ -1,12 +1,20 @@
 class Vertex
+  attr_accessor :value, :in_edges, :out_edges
   def initialize(value)
-
+    @value = value
+    @in_edges = []
+    @out_edges = []
   end
 end
 
 class Edge
+  attr_accessor :from_vertex, :to_vertex, :cost
   def initialize(from_vertex, to_vertex, cost = 1)
-
+    @from_vertex = from_vertex
+    @from_vertex.out_edges << self
+    @to_vertex = to_vertex
+    @to_vertex.in_edges << self
+    @cost = cost
   end
 
   def destroy!
