813eb7ec021df56c843d350c71f70c4afb48c3b1
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-07 23:01:08 -0700

    Update topological sort

--- yujen_chang_6/lib/topological_sort.rb
+++ yujen_chang_6/lib/topological_sort.rb
@@ -2,31 +2,28 @@ require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
+# Kahn's Algorithm
 def topological_sort(vertices)
   queue = []
   sorted = []
 
-
   vertices.each do |vertex|
-
     queue << vertex if vertex.in_edges == []
-
   end
 
-
   until queue.empty?
+    edges = []
     current_vertex = queue.shift
     sorted << current_vertex
-
 
     current_vertex.out_edges.each do |edge|
       edge.to_vertex.in_edges.delete(edge)
       if edge.to_vertex.in_edges.empty?
         queue << edge.to_vertex
       end
-      #  can't have this destroy, otherwise it would fail
-      # edge.destroy!
+      edges << edge
     end
+    edges.each {|edge| edge.destroy!}
     # if we remove from graph then we need to check the length of vertices (=0) to make sure all vertices are sorted
     # vertices.delete(current_vertex)
   end
@@ -38,3 +35,27 @@ def topological_sort(vertices)
   end
 
 end
+
+# #  Tarjan's Algorithm
+# def topological_sort(vertices)
+#   sorted = []
+#   visited = []
+#
+#   vertices.each do |vertex|
+#     sorted = visit(vertex, sorted, visited)
+#   end
+#   sorted
+#
+# end
+#
+# def visit(vertex, sorted, visited)
+#   return sorted if visited.include?(vertex)
+#
+#   vertex.out_edges.each do |edge|
+#     # return[] if visited.include?(edge.to_vertex)
+#     sorted = visit(edge.to_vertex, sorted, visited)
+#   end
+#
+#   visited << vertex
+#   sorted.unshift(vertex)
+# end
