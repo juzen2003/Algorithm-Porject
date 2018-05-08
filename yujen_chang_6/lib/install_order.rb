e5128fd7eef15b00210f8fdd94590a5cfff3add9
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-07 20:29:03 -0700

    Done install order

--- yujen_chang_6/lib/install_order.rb
+++ yujen_chang_6/lib/install_order.rb
@@ -7,8 +7,25 @@
 
 # Import any files you need to
 
-
+require_relative 'graph'
+require_relative 'topological_sort'
 
 def install_order(arr)
+  no_dependencies = (1..arr.flatten.max).to_a - arr
 
+  hsh = {}
+  arr.each do |package|
+    hsh[package[0]] = Vertex.new(package[0]) if hsh[package[0]] == nil
+    hsh[package[1]] = Vertex.new(package[1]) if hsh[package[1]] == nil
+    Edge.new(hsh[package[1]], hsh[package[0]])
+  end
+
+  no_dependencies.each do |package|
+    hsh[package] = Vertex.new(package) if hsh[package] == nil
+  end
+
+  sorted = topological_sort(hsh.values)
+  sorted.map do |el|
+    el.value
+  end
 end
